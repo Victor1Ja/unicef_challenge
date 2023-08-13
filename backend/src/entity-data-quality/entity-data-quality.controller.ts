b331498d8119b9a778a2e822c37f24f8f9a242b1
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-08-13 16:17:55 -0400

    fix: Update entity_data_quality

--- backend/src/entity-data-quality/entity-data-quality.controller.ts
+++ backend/src/entity-data-quality/entity-data-quality.controller.ts
@@ -1,20 +1,5 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { EntityDataQualityService } from './entity-data-quality.service';
-import {
-  EntityDataQualityDto,
-  EditEntityDataQualityDto,
-} from './dto/entity-data-quality.dto';
 
 @Controller('entity-data-quality')
 export class EntityDataQualityController {
@@ -25,37 +10,11 @@ export class EntityDataQualityController {
     return this.entityDataQualityService.getAllEntityDataQualities();
   }
 
-  @Post()
-  createEntityDataQuality(@Body() dto: EntityDataQualityDto) {
-    return this.entityDataQualityService.createEntityDataQuality(dto);
-  }
-
   @Get(':id')
   getEntityDataQualityById(
     @Param('id', ParseIntPipe) entityDataQualityId: number,
   ) {
     return this.entityDataQualityService.getEntityDataQualityById(
-      entityDataQualityId,
-    );
-  }
-
-  @Patch(':id')
-  editEntityDataQualityById(
-    @Param('id', ParseIntPipe) entityDataQualityId: number,
-    @Body() dto: EditEntityDataQualityDto,
-  ) {
-    return this.entityDataQualityService.editEntityDataQualityById(
-      entityDataQualityId,
-      dto,
-    );
-  }
-
-  @HttpCode(HttpStatus.NO_CONTENT)
-  @Delete(':id')
-  deleteEntityDataQualityById(
-    @Param('id', ParseIntPipe) entityDataQualityId: number,
-  ) {
-    return this.entityDataQualityService.deleteEntityDataQualityById(
       entityDataQualityId,
     );
   }
