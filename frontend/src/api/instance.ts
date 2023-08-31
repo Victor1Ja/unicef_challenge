2909f824bcaf266f50d4349a79bd013239854223
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-08-31 04:16:32 -0400

    feat:

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const baseUrl = process.env.REACT_APP_API_URL;
+export const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:3333';
 
 export const instance = axios.create({});
